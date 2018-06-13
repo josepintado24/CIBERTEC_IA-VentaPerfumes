35309a6d488b7461c1df88108ca29bba14b16609
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2018-06-13 09:28:04 -0500

    Estructura del Proyecto

---
+++ VentaPerfumes/src/datosProyecto/BDPerfumes.java
@@ -0,0 +1,51 @@
+package datosProyecto;
+
+public class BDPerfumes {
+	
+	// Datos mínimos del primer perfume
+	public static String nombre0 = "ALCAZAR";
+	public static double precio0 = 135.0;
+	public static double contenido0 = 100;
+	public static int año0 = 2014;
+	public static String casa0 = "Ted Lapidus";
+	// Datos mínimos del segundo perfume
+	public static String nombre1 = "ALLURE";
+	public static double precio1 = 390.0;
+	public static double contenido1 = 50.0;
+	public static int año1 = 1998;
+	public static String casa1 = "Chanel";
+	// Datos mínimos del tercer perfume
+	public static String nombre2 = "AQUA RUSH";
+	public static double precio2 = 62.0;
+	public static double contenido2 = 90.0;
+	public static int año2 = 2012;
+	public static String casa2 = "Nautica";
+	// Datos mínimos del cuarto perfume
+	public static String nombre3 = "ENVY";
+	public static double precio3 = 510.0;
+	public static double contenido3 = 7.5;
+	public static int año3 = 1997;
+	public static String casa3 = "Gucci";
+	// Datos mínimos del quinto perfume
+	public static String nombre4 = "ALAIA";
+	public static double precio4 = 210.0;
+	public static double contenido4 = 30.0;
+	public static int año4 = 2015;
+	public static String casa4 = "Azzedine";
+	// Porcentajes de descuento
+	public static double porcentaje1 = 4.0;
+	public static double porcentaje2 = 5.5;
+	public static double porcentaje3 = 7.0;
+	public static double porcentaje4 = 8.5;
+	// Cantidad óptima de perfumes vendidos
+	public static int cantidadOptima = 40;
+	// Importe a pagar mínimo para obtener el obsequio
+	public static int importeMinimoObsequiable = 750;
+	// Obsequio por importe mínimo
+	public static String obsequio = "Billetera";
+	// Número de cliente que recibe el premio sorpresa
+	public static int numeroClienteSorpresa = 5;
+	// Premio sorpresa
+	public static String premioSorpresa = "Un polo";
+
+}
